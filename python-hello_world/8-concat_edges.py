28c7c5e6dc69d10bae50cd894cf6fa91b1c64b3d
mathpvx <7547@holbertonstudents.com>
2024-01-23 13:20:25 +0100

    print splited sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+str = str.split()
+print(str[5] + " " + str[6] + " " + str[12] + " " + str[0])
