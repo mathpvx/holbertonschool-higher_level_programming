3cb3b7f6e4626803bd01d2ea44a5b857d36b1dfd
mathpvx <7547@holbertonstudents.com>
2024-01-24 14:54:11 +0100

    shortened the code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-i = 99
-print("{:02d}".format(i), end='')
+for i in range(0, 100):
+    print("{:02d}".format(i), end=', ' if i < 99 else '')
