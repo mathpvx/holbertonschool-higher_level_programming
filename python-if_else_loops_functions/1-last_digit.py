3584a309499f1e5a91edfb0bf320938aeec95784
mathpvx <7547@holbertonstudents.com>
2024-01-24 12:08:19 +0100

    take in account negative last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 last_d = number % 10
-if last_d > 5:
-    print(f"Last digit of {number} is {last_d} and is greater than 5\n")
+neg_last_d = number % -10
+if number > 0 and last_d > 5:
+    print(f"Last digit of {number} is {last_d} and is greater than 5")
 elif last_d == 0:
-    print(f"Last digit of {number} is {last_d} and is 0\n")
-elif last_d < 6:
-    print(f"Last digit of {number} is {last_d} and is less than 6 and not 0\n")
+    print(f"Last digit of {number} is {last_d} and is 0")
+elif number > 0 and last_d < 6 and last_d > 0:
+    print(f"Last digit of {number} is {last_d} and is less than 6 and not 0")
+elif number < 0 and last_d < 6 and last_d > 0:
+    print(f"Last digit of {number} is {neg_last_d*-1} and is less than 6 and not 0")
+elif number >= 0 and last_d > 5:
+    print(f"Last digit of {number} is {last_d} and is greater than 5")
