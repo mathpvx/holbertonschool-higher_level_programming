341c5937fba25deed986ab276d8f615b794ccefe
mathpvx <7547@holbertonstudents.com>
2024-01-26 09:55:07 +0100

    correct indentation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
