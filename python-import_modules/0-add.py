840238455caf9b5c3cc67ab9c4befa831dd51c0e
mathpvx <7547@holbertonstudents.com>
2024-01-26 09:45:41 +0100

    add if statement

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
