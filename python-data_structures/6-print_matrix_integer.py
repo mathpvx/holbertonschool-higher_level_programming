3b93f094d71f1cc7d5c46394bf2d79c732e166bd
mathpvx <7547@holbertonstudents.com>
2024-01-30 13:22:10 +0100

    prints a matrix of int

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range (len(matrix)):
+        for j in range (len(matrix[i])):
+            if j == len(matrix) - 1:
+                print("{:d}".format(matrix[i][j], end=''))
+            else:
+                print("{:d} ".format(matrix[i][j]), end='')
