df38a7e31c4b57d17363e2bc46a12371dcc9f603
mathpvx <7547@holbertonstudents.com>
2024-01-31 14:54:54 +0100

    pycode style checked

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:
-        if i %2 == 0:
+        if i % 2 == 0:
             new_list += [True]
         else:
             new_list += [False]
