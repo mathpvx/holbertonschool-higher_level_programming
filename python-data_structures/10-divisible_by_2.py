d120d325ff37f981481a71ae70517930ae48a15c
mathpvx <7547@holbertonstudents.com>
2024-01-31 14:53:06 +0100

    checks if each element in a list is a multiple of a 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in my_list:
+        if i %2 == 0:
+            new_list += [True]
+        else:
+            new_list += [False]
+    return new_list
