a9bdd748410be2bfb13b1afce15c7b3772a63f93
mathpvx <7547@holbertonstudents.com>
2024-02-02 12:46:38 +0100

    prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except Exception as e:
+            break
+        else:
+            count += 1
+    print()
+    return count
