b7995af6888f90e211d6c03dd83b391988134729
mathpvx <7547@holbertonstudents.com>
2024-02-02 12:56:05 +0100

    fixed pycode style with except clause

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,10 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
+        except (TypeError, ValueError):
+            pass
         except IndexError:
             raise
-        except:
-            pass
         else:
             count += 1
     print()
