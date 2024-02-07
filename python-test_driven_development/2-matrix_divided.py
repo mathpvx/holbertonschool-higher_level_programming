c98abe15e0b1df8c912fd7d54c478458611f9f02
mathpvx <7547@holbertonstudents.com>
2024-02-07 16:33:22 +0100

    divides all elements of a matrix handling error cases

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,19 @@
+def matrix_divided(matrix, div):
+    
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if not matrix or not all(row for row in matrix) or not all(isinstance(item, (int, float)) for row in matrix for item in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    new_matrix = [[round(item / div, 2) for item in row] for row in matrix]
+    
+    return new_matrix
