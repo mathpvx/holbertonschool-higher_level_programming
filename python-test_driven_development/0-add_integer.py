7c8bcbe6f223b02cec2e77e066b821e2f405b3f4
mathpvx <7547@holbertonstudents.com>
2024-02-08 10:24:12 +0100

    pycode style fixed and docstrings added

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Adds two integers together."""
+
+
 def add_integer(a, b=98):
+    """Adds two integers or floats together. If the inputs are floats, they are
+    converted to integers before addition.
+    Returns: The sum of a and b.
+    Raises: TypeError: If a or b are not integers or floats."""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
