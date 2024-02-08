3b1897efad0b03760771346cf61e69be1197bd49
mathpvx <7547@holbertonstudents.com>
2024-02-08 10:58:17 +0100

    docstrings

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """Prints a string with the format "My name is <first name> <last name>".
+    Raises: TypeError: If first_name or last_name are not strings."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
