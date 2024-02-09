57dbd770cc331224ed23d6d416059bda7cce706c
mathpvx <7547@holbertonstudents.com>
2024-02-09 14:18:19 +0100

    defines a rectangle with width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class Rectangle:
+    """defining a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """This method constructs the object"""
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """func defintion"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """func defintion"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """func defintion"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """func defintion"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
