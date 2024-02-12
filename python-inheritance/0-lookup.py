cbb79a0241a2a056eb77d0dd5c45357247f54a67
mathpvx <7547@holbertonstudents.com>
2024-02-12 16:38:13 +0100

    flake8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,8 @@
 Module with the method lookup
 =============================
 """
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
     return dir(obj)
