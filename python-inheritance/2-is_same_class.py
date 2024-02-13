542b153889439c03ad8b691a988c1a48166ade50
mathpvx <7547@holbertonstudents.com>
2024-02-13 09:46:48 +0100

    add module comment

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+================================
+module with method is_same_class
+================================
+"""
+
+
 def is_same_class(obj, a_class):
-    """Function that returns True if object is an instance of the input class """
-    if type(obj) == a_class:
+    """Function that returns True
+    if object is an instance of the input class """
+    if type(obj) is a_class:
         return True
     else:
         return False
