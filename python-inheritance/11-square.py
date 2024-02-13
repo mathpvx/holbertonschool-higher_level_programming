3836995e10783ca31568cfb124baaf686c645224
mathpvx <7547@holbertonstudents.com>
2024-02-13 13:44:54 +0100

    returns description of the square with str method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+===================================
+module with class Rectangle
+===================================
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """constructs with size attribute"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area method"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """returns description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
