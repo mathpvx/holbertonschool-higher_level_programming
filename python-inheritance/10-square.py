d3da4e55d5a80f3a5c7f2629cb9a47f54980f1e7
mathpvx <7547@holbertonstudents.com>
2024-02-13 13:32:54 +0100

    added docstrings

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""
-===================================
-module with class Rectangle
-===================================
-"""
 Rectangle = __import__('9-rectangle').Rectangle
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
 
 
 class Square(Rectangle):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, size):
+        """ constructor with size attribute """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        return self.__size ** 2
+        """ area method"""
+        return self.__size**2
