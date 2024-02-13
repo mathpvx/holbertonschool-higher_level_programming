6f8eb7cc8c0e660e876a81aa9175f3a817d7df5f
mathpvx <7547@holbertonstudents.com>
2024-02-13 13:37:20 +0100

    add linebreaks

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,10 +12,12 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """constructs with size attribute"""
+        
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
         """area method"""
+        
         return self.__size ** 2
