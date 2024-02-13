3bde2e536fa1d250a9621daae6ade1614ae7113f
mathpvx <7547@holbertonstudents.com>
2024-02-13 09:44:37 +0100

    edit is to ==

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     """Function that returns True if object is an instance of the input class """
-    if type(obj) is a_class:
+    if type(obj) == a_class:
         return True
     else:
         return False
