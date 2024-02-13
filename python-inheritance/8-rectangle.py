069365ab2755774f4cd437a1ac3599c58731ae10
mathpvx <7547@holbertonstudents.com>
2024-02-13 12:38:43 +0100

    modif on code structure

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """
 ===================================
 module with class Rectangle
 ===================================
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
