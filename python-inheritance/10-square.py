50597590b3ca8d9d5d535a0237ddc9230678adf3
mathpvx <7547@holbertonstudents.com>
2024-02-13 13:34:22 +0100

    modified the code structure

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""
+===================================
+module with class Rectangle
+===================================
+"""
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Square(Rectangle):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, size):
-        """ constructor with size attribute """
+        """constructs with size attribute"""
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """ area method"""
+        """area method"""
         return self.__size**2
