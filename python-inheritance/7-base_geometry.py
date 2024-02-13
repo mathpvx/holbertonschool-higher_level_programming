4622acd40cf33a307f0337868bf7caa4333734e4
mathpvx <7547@holbertonstudents.com>
2024-02-13 11:25:45 +0100

    modify code and doctest as there are checkers issues

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
+
 """
-===================================
-module with class BaseGeometry
-===================================
+This module defines the `BaseGeometry` class.
 """
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
-
+    """
+    BaseGeometry class.
+    """
     def area(self):
-        """method for calculated area"""
+        """ Raises Exception as area is not implemented """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Method for validate if a num is integer"""
-
-        if type(value) is not int:
+        """
+        validates value
+        """
+        if type(value) is not (int):
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
