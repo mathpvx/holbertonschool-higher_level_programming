e780a6c12abee5fcc8bce52c47882e5dbd90c052
mathpvx <7547@holbertonstudents.com>
2024-02-14 12:39:57 +0100

    remove the extra break line at the end

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,4 +3,4 @@ def read_file(filename="my_file_0.txt"):
     """ Reads the contents of a text file and prints it to the console."""
     with open('my_file_0.txt', 'r', encoding='utf-8') as file:
         data = file.read()
-        print(data)
+        print(data, end='')
