72e93efebe985d363f0f503abf06645f81998437
mathpvx <7547@holbertonstudents.com>
2024-02-14 12:37:31 +0100

    read the content of a file and prints it in utf8

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def read_file(filename="my_file_0.txt"):
+    """ Reads the contents of a text file and prints it to the console."""
+    with open('my_file_0.txt', 'r', encoding='utf-8') as file:
+        data = file.read()
+        print(data)
