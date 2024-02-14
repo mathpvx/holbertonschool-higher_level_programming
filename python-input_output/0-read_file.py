95993e0c06ec9dc663a9e396a8a279e6ffb9e5e0
mathpvx <7547@holbertonstudents.com>
2024-02-14 12:43:06 +0100

    add docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Read file function
+"""
+
+
 def read_file(filename=""):
     """ Reads the contents of a text file and prints it to the console."""
     with open(filename, 'r', encoding='utf-8') as file:
