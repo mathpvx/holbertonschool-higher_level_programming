9aef3366a4a0d32b699f323b810fe9dc905a16aa
mathpvx <7547@holbertonstudents.com>
2024-02-14 12:41:51 +0100

    removed the file path as can be executed with any test file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def read_file(filename="my_file_0.txt"):
+def read_file(filename=""):
     """ Reads the contents of a text file and prints it to the console."""
-    with open('my_file_0.txt', 'r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
         print(data, end='')
