a7fac7175ecfae64449be1f40da8662f6acaf742
mathpvx <7547@holbertonstudents.com>
2024-02-15 18:14:44 +0100

    modified the code

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,12 +5,16 @@ Pascal's Triangle Module
 
 
 def pascal_triangle(n):
-    """calculate pascal"""
-    my_list = []
-    if n <= 0:
-        return my_list
-    for i in range(n):
-        num = 11**i
-        li = [int(n) for n in str(num)]
-        my_list.append(li)
-    return my_list
+    """lists of integers representing the Pascal’s triangle of n"""
+    if n > 0:
+        l_pascal = [[1]]
+        for i in range(n - 1):
+            my_list = [1]
+            if i > 0:
+                n = len(l_pascal)
+                for j in range(n - 1):
+                    my_list.append(l_pascal[i][j] + l_pascal[i][j + 1])
+            my_list.append(1)
+            l_pascal.append(my_list)
+        return l_pascal
+    return []
