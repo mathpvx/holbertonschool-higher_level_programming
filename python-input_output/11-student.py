f540940ff1c1d7b9fcd786bd13c1852eb49bfc1b
mathpvx <7547@holbertonstudents.com>
2024-02-15 18:06:55 +0100

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Class Module
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in attrs:
+                if isinstance(i, str) and hasattr(self, i):
+                    new_dict[i] = getattr(self, i)
+            return new_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
