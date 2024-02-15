2c46569d1cac889535d26bbfc146dd2b76fb6fd0
mathpvx <7547@holbertonstudents.com>
2024-02-15 18:09:30 +0100

    returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle Module
+"""
+
+
+def pascal_triangle(n):
+    """calculate pascal"""
+    my_list = []
+    if n <= 0:
+        return my_list
+    for i in range(n):
+        num = 11**i
+        li = [int(n) for n in str(num)]
+        my_list.append(li)
+    return my_list
