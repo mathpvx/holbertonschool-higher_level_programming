ab3752b5ea2c56dfec6c1af1046331f06d619ef1
mathpvx <7547@holbertonstudents.com>
2024-02-20 11:53:12 +0100

    Base class for managing id attribute in all future classes

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Base class for managing id attribute in all future classes.
+"""
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a Base instance. If id is not provided, generated automatically."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
