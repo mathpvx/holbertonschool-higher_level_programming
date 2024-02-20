bc72985b2b9d9ed14eca5fdaa647087cba9006b6
mathpvx <7547@holbertonstudents.com>
2024-02-20 11:56:05 +0100

    pep 8 fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,12 +2,15 @@
 """
 Base class for managing id attribute in all future classes.
 """
+
+
 class Base:
     """Represent the base model."""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a Base instance. If id is not provided, generated automatically."""
+        """Initialize a Base instance.
+        If id is not provided, generated automatically."""
         if id is not None:
             self.id = id
         else:
