acc5971da05f5ea10e988ab0f0096571a1d60581
mathpvx <7547@holbertonstudents.com>
2024-02-21 10:34:33 +0100

    adding the public method def display(self): that prints in stdout the Rectangle instance with the character #

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate the area of the rectangle"""
         return self.width * self.height
+
+    def display(self):
+        """Display the rectangle with '#' characters"""
+        for _ in range(self.height):
+            print("#" * self.width)
