acd671201a7fcb6558b902f0b7a73950907531f1
mathpvx <7547@holbertonstudents.com>
2024-02-22 17:34:27 +0100

    added  class method def load_ from_file with restructuration of the code to make it work

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""
-Base class for managing id attribute in all future classes.
-"""
+"""A module with a simple class"""
+
 import json
 
 
 class Base:
-    """Represent the base model."""
+    """This class will be the “base” of all other classes in this project.
+    The goal of it is to manage id attribute in all your future classes and
+    to avoid duplicating the same code (by extension, same bugs)
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a Base instance.
-        If id is not provided, generated automatically."""
+        """Initializes a Base instance"""
         if id is not None:
             self.id = id
         else:
@@ -20,28 +21,58 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+
+        my_dict = []
+        for obj in list_objs:
+            my_dict.append(obj.to_dictionary())
+        obj = cls.to_json_string(my_dict)
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            jsonfile.write(obj)
 
     @staticmethod
     def from_json_string(json_string):
-        """Return list represented by json_string"""
-        if json_string is None or len(json_string) == 0:
+        """Returns the list of dictionaries represented by json_string"""
+        if json_string is None:
             return []
         else:
             return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Create and return an instance with all attributes already set"""
+        """Returns an instance with all attributes already set"""
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)  # Create a dummy Rectangle instance
-        elif cls.__name__ == "Square":
-            dummy = cls(1)  # Create a dummy Square instance
-        else:
-            return None
-        dummy.update(**dictionary)  # Update attributes using dictionary
+            dummy = cls(2, 2)
+        if cls.__name__ == "Square":
+            dummy = cls(7)
+
+        dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of classes instantiated from a file of JSON strings.
+
+        Reads from `<cls.__name__>.json`.
+
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
