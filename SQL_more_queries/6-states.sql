2176937569fe44433ba11bf62a937271fe2076ab
mathpvx <7547@holbertonstudents.com>
2024-03-20 09:49:15 +0100

    changed the syntax

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	name VARCHAR(256) != ""
-)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
