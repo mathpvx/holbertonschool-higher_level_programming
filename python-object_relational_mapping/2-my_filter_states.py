e463b0f886f092234866a2a91f530eaa6c26ff8c
mathpvx <7547@holbertonstudents.com>
2024-03-20 21:49:14 +0100

    pycodestyle and docstrings

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches argument
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+""" Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches argument
+Usage: ./2-my_filter_states.py <mysql username>
+<mysql password> <database name> <state name searched>"""
+
+
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
         user=sys.argv[1],
         password=sys.argv[2],
         db=sys.argv[3])
-    
+
     cursor = db.cursor()
     cursor.execute("SELECT * \
                  FROM `states` \
