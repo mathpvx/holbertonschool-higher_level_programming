c2d3a76e92d465ef57454f03a3ee3a2ed568d1d8
mathpvx <7547@holbertonstudents.com>
2024-03-20 14:51:49 +0100

    added localhsot and port

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         user=sys.argv[1],
         password=sys.argv[2],
-        database=sys.argv[3]
+        database=sys.argv[3],
+        host="localhost",
+        port=3306
         )
     # instantiate a cursor
     cursor = db.cursor()
