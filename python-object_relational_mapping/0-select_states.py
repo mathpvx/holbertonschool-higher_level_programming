69b87d5a947914a778e6ecf2a159408930147bc6
mathpvx <7547@holbertonstudents.com>
2024-03-20 16:19:35 +0100

    k

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa
-"""
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
 
 
 import MySQLdb
@@ -8,29 +7,24 @@ import sys
 
 
 if __name__ == "__main__":
-    """Connect to the database"""
-    db = MySQLdb.connect(
+    """including the guard clause so that the code is only
+    executed when the script is run directly
+    """
+
+    """establish a connection to the MySQLdb database"""
+    connection = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3])
 
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the SQL query
+    """calling the cursor method that will be used to execute SQL
+    queries and fetch results from the database
+    """
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
-
-    # Fetch all the rows
-    results = cursor.fetchall()
-
-    # Print the results
-    for row in results:
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-
-    # Close the cursor and database connection
-    if 'cursor' in locals():
-        cursor.close()
-    if 'db' in locals():
-        db.close()
+    connection.close()
