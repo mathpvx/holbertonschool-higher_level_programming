025484cd53ad9252164fa63088c214bd0777b1a7
mathpvx <7547@holbertonstudents.com>
2024-03-20 16:37:52 +0100

    revert prev v

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+""" Lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username>
+<mysql password> <database name>"""
 
 
 import MySQLdb
@@ -7,24 +9,22 @@ import sys
 
 
 if __name__ == "__main__":
-    """including the guard clause so that the code is only
-    executed when the script is run directly
-    """
+    """execute only if from python interpreter itself"""
 
-    """establish a connection to the MySQLdb database"""
-    connection = MySQLdb.connect(
+    """ co to DB"""
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3])
 
-    """calling the cursor method that will be used to execute SQL
-    queries and fetch results from the database
+    """fetchall() returns a list of tuples of each row
+    iterates on each tuple and prints
     """
-    cursor = connection.cursor()
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    connection.close()
+    db.close()
