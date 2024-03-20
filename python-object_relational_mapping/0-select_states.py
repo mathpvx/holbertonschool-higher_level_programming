188e42b59a3dccb288b1ec60e0814f2f22adca59
mathpvx <7547@holbertonstudents.com>
2024-03-20 19:55:00 +0100

    deleted variable

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -24,8 +24,7 @@ if __name__ == "__main__":
     """
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
     cursor.close()
     db.close()
