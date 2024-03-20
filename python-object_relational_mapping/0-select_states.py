3ae1119986b335a98360a1a7a8e93943070c3185
mathpvx <7547@holbertonstudents.com>
2024-03-20 15:37:34 +0100

    modified arg order

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     """ lists of arg taken when executing"""
 
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
-        database=sys.argv[3],
-        host="localhost",
-        port=3306
+        database=sys.argv[3]
         )
     """ instantiate a cursor"""
     cursor = db.cursor()
