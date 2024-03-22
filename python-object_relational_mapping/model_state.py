c13bf8196d58aff7e50e0a41a145f3979ec90797
mathpvx <7547@holbertonstudents.com>
2024-03-22 21:40:41 +0100

    code restruct

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python3
 """
 Contains the class definition of a State using SQLAlchemy ORM.
 """
+
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(Base):
-    """State class for representing states in a database."""
 
+class State(Base):
+    """State class"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
